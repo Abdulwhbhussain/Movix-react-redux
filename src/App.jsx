5f3027f78d735bd33ffac4107de48657cbe3e0e8
WHB901 <abdulwhbhussain@gmail.com>
2023-06-19 16:52:42 +0500

    initial file and Folder Structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import './App.css';
 import { fetchDataFromApi } from './utils/api';
 import { useDispatch, useSelector } from 'react-redux';
 import { getApiConfiguration } from './redux/homeSlice';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Home from './pages/home/Home';
+import Details from './pages/details/Details';
+import SearchResult from './pages/searchResult/SearchResult';
+import Explore from './pages/explore/Explore';
+import PageNotFound from './pages/404/PageNotFound';
 
 function App() {
   const { url } = useSelector((state) => state.home);
---
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.scss'
+
+const HeroBanner = () => {
+  return (
+    <div>HeroBanner</div>
+  )
+}
+
+export default HeroBanner
