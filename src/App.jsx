b35d02f1dd4a33acbb3ebb68e769f4519a2cb9f2
WHB901 <abdulwhbhussain@gmail.com>
2023-06-19 14:08:16 +0500

    api function tested

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
-import './App.css'
+import React, { useEffect } from 'react';
+import './App.css';
+import { fetchDataFromApi } from './utils/api';
 
 function App() {
+
+  const apiTesting = () => {
+    fetchDataFromApi('/movie/popular').then((data) => {
+      console.log(data);
+    })
+  }
+
+  useEffect(() => {
+    apiTesting();
+  }, [])
 
   return (
     <div>
